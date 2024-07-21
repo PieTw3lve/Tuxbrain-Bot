367b926408cc147161c126c403b9a6078625218c
PieTw3lve <ericlim000@outlook.com>
2024-07-21 16:41:44 -0400

    Some messages were not ephemeral

--- src/extensions/owner/ticket.py
+++ src/extensions/owner/ticket.py
@@ -22,10 +22,10 @@ async def ticket(ctx: lightbulb.Context) -> None:
 @lightbulb.command('set', "Set a server member's wallet to a specific amount.", pass_options=True)
 @lightbulb.implements(lightbulb.SlashSubCommand)
 async def set_ticket(ctx: lightbulb.Context, user: hikari.User, amount: int):
-    if user.is_bot: # checks if the user is a bot
+    if user.is_bot:
         embed = hikari.Embed(description='You are not allowed to set tickets to this user!', color=get_setting('general', 'embed_error_color'))
         return await ctx.respond(embed, flags=hikari.MessageFlag.EPHEMERAL)
-    elif verify_user(user) == None: # if user has never been register
+    elif verify_user(user) == None:
         register_user(ctx.user)
 
     economy.set_ticket(user.id, amount)
@@ -39,14 +39,14 @@ async def set_ticket(ctx: lightbulb.Context, user: hikari.User, amount: int):
 @lightbulb.command('add', "Add tickets to a server member's wallet.", pass_options=True)
 @lightbulb.implements(lightbulb.SlashSubCommand)
 async def add(ctx: lightbulb.Context, user: hikari.User, amount: int):
-    if user.is_bot: # checks if the user is a bot
+    if user.is_bot:
         embed = hikari.Embed(description='You are not allowed to add tickets to this user!', color=get_setting('general', 'embed_error_color'))
         return await ctx.respond(embed, flags=hikari.MessageFlag.EPHEMERAL)
-    elif verify_user(user) == None: # if user has never been register
+    elif verify_user(user) == None:
         register_user(ctx.user)
     
     if economy.add_ticket(user.id, amount):
-        embed = (hikari.Embed(description=f"You added {amount:,} 🎟️ to {user.global_name}'s wallet!", color=get_setting('general', 'embed_color')))
+        embed = (hikari.Embed(description=f"You added {amount:,} 🎟️ to {user.global_name}'s wallet.", color=get_setting('general', 'embed_color')))
         await ctx.respond(embed, flags=hikari.MessageFlag.EPHEMERAL)
 
 @ticket.child
@@ -56,15 +56,15 @@ async def add(ctx: lightbulb.Context, user: hikari.User, amount: int):
 @lightbulb.command('remove', "Remove tickets from a server member's wallet.", pass_options=True)
 @lightbulb.implements(lightbulb.SlashSubCommand)
 async def remove(ctx: lightbulb.Context, user: hikari.User, amount: int, update: bool):
-    if user.is_bot: # checks if the user is a bot
+    if user.is_bot:
         embed = hikari.Embed(description='You are not allowed to take money from this user!', color=get_setting('general', 'embed_error_color'))
         return await ctx.respond(embed, flags=hikari.MessageFlag.EPHEMERAL)
-    elif verify_user(user) == None: # if user has never been register
+    elif verify_user(user) == None:
         register_user(ctx.user)
     
     if economy.remove_ticket(user.id, amount):
-        embed = (hikari.Embed(description=f"You took {amount:,} 🎟️ from {user.global_name}'s wallet!", color=get_setting('general', 'embed_color')))
-        await ctx.respond(embed)
+        embed = (hikari.Embed(description=f"You took {amount:,} 🎟️ from {user.global_name}'s wallet.", color=get_setting('general', 'embed_color')))
+        await ctx.respond(embed, flags=hikari.MessageFlag.EPHEMERAL)
     else:
         embed = (hikari.Embed(description=f"That amount exceeds {user.global_name}'s wallet!", color=get_setting('general', 'embed_error_color')))
         await ctx.respond(embed, flags=hikari.MessageFlag.EPHEMERAL)
--- src/extensions/owner/wallet.py
+++ src/extensions/owner/wallet.py
@@ -22,10 +22,10 @@ async def wallet(ctx: lightbulb.Context) -> None:
 @lightbulb.command('set', "Set a server member's wallet to a specific amount.", pass_options=True)
 @lightbulb.implements(lightbulb.SlashSubCommand)
 async def set_wallet(ctx: lightbulb.Context, user: hikari.User, currency: str, amount: int):
-    if user.is_bot: # checks if the user is a bot
+    if user.is_bot:
         embed = hikari.Embed(description='You are not allowed to set money to this user!', color=get_setting('general', 'embed_error_color'))
         return await ctx.respond(embed, flags=hikari.MessageFlag.EPHEMERAL)
-    elif verify_user(user) == None: # if user has never been register
+    elif verify_user(user) == None:
         register_user(ctx.user)
 
     economy.set_money(user.id, amount)
@@ -40,14 +40,14 @@ async def set_wallet(ctx: lightbulb.Context, user: hikari.User, currency: str, a
 @lightbulb.command('add', "Add coins to a server member's wallet.", pass_options=True)
 @lightbulb.implements(lightbulb.SlashSubCommand)
 async def add(ctx: lightbulb.Context, user: hikari.User, amount: int, update: bool):
-    if user.is_bot: # checks if the user is a bot
+    if user.is_bot:
         embed = hikari.Embed(description='You are not allowed to add money to this user!', color=get_setting('general', 'embed_error_color'))
         return await ctx.respond(embed, flags=hikari.MessageFlag.EPHEMERAL)
-    elif verify_user(user) == None: # if user has never been register
+    elif verify_user(user) == None:
         register_user(ctx.user)
     
     if economy.add_money(user.id, amount, update):
-        embed = (hikari.Embed(description=f"You added 🪙 {amount:,} to {user.global_name}'s wallet!", color=get_setting('general', 'embed_color')))
+        embed = (hikari.Embed(description=f"You added 🪙 {amount:,} to {user.global_name}'s wallet.", color=get_setting('general', 'embed_color')))
         return await ctx.respond(embed, flags=hikari.MessageFlag.EPHEMERAL)
 
 @wallet.child
@@ -58,15 +58,15 @@ async def add(ctx: lightbulb.Context, user: hikari.User, amount: int, update: bo
 @lightbulb.command('remove', "Remove coins from a server member's wallet.", pass_options=True)
 @lightbulb.implements(lightbulb.SlashSubCommand)
 async def remove(ctx: lightbulb.Context, user: hikari.User, amount: int, update: bool):
-    if user.is_bot: # checks if the user is a bot
+    if user.is_bot:
         embed = hikari.Embed(description='You are not allowed to take money from this user!', color=get_setting('general', 'embed_error_color'))
         return await ctx.respond(embed, flags=hikari.MessageFlag.EPHEMERAL)
-    elif verify_user(user) == None: # if user has never been register
+    elif verify_user(user) == None:
         register_user(ctx.user)
     
     if economy.remove_money(user.id, amount, update):
-        embed = (hikari.Embed(description=f"You took 🪙 {amount:,} from {user.global_name}'s wallet!", color=get_setting('general', 'embed_color')))
-        await ctx.respond(embed)
+        embed = (hikari.Embed(description=f"You took 🪙 {amount:,} from {user.global_name}'s wallet.", color=get_setting('general', 'embed_color')))
+        await ctx.respond(embed, flags=hikari.MessageFlag.EPHEMERAL)
     else:
         embed = (hikari.Embed(description=f"That amount exceeds {user.global_name}'s wallet!", color=get_setting('general', 'embed_error_color')))
         await ctx.respond(embed, flags=hikari.MessageFlag.EPHEMERAL)
